0ac56912b58c739ca16f8c30ebe877950847badc
DanielBeaver1000100 <daniel.beaver1@marist.edu>
2024-05-08 08:52:16 -0400

    change 2

--- Bot/XLSearch.py
+++ Bot/XLSearch.py
@@ -1,7 +1,7 @@
 import pyperclip
 import openpyxl
 
-def print_first_name_from_excel(file_path, column):
+def print_first_name_from_excel(file_path, column, skip=1):
     try:
         # Load the Excel workbook
         wb = openpyxl.load_workbook(file_path)
@@ -11,19 +11,20 @@ def print_first_name_from_excel(file_path, column):
         
         # Iterate through each row in the specified column and print the first name
         for i, cell in enumerate(ws[column], start=1):
-            Title = cell.value
-            try:
-                First_Name=Title.split()[1] 
-                 # Split the  name and get the part
-            except:
-                First_Name="*"
-            try:
-               Last_name = Title.split()[2] 
-            except:
-                Last_name="*" 
-            print(f"#{i}: {First_Name , Last_name}")
-            pyperclip.copy(Last_name)
-            input("Press Enter to continue to the next row...")
+            if i>=skip:
+                Title = cell.value
+                try:
+                    First_Name=Title.split()[1] 
+                    # Split the  name and get the part
+                except:
+                    First_Name="*"
+                try:
+                    Last_name = Title.split()[2] 
+                except:
+                    Last_name="*" 
+                print(f"#{i}: {First_Name , Last_name}")
+                pyperclip.copy(Last_name)
+                input("Press Enter to continue...")
     
     except FileNotFoundError:
         print("File not found.")
@@ -33,4 +34,5 @@ def print_first_name_from_excel(file_path, column):
 # Example usage
 file_path = r"c:\Users\danie\Desktop\Copy of AllDevices_May2024.xlsx"  # Update this with your file path
 column = "D"  # Update this with the column you want to print
-print_first_name_from_excel(file_path, column)
+start=int(input("Enter starting row:"))
+print_first_name_from_excel(file_path, column, start)
